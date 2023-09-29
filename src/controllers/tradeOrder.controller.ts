ad740a9948553ad2159b29c9af48902667993cf2
Carlos Casas <carloscasasramirez@gmail.com>
2023-09-29 10:20:54 -0500

    cambio

--- src/controllers/tradeOrder.controller.ts
+++ src/controllers/tradeOrder.controller.ts
@@ -112,7 +112,6 @@ export class TradeOrder {
     static getCustomer = async (req: Request, res: Response): Promise<Response> => {
 
         try {
-
             const conn = await connect();
             const limit = Number(req.query.limit) || 10;
             const page = Number(req.query.page) || 1;
