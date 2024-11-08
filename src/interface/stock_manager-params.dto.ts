dcddb22d087596506707bf2eda6b6cce96bfab44
Desarrollosyscomp <desarrollosyscomp@gmail.com>
2024-11-08 16:37:48 -0500

    crear parametros para editar precios y descuentos en pedidos

--- src/interface/stock_manager-params.dto.ts
+++ src/interface/stock_manager-params.dto.ts
@@ -2,16 +2,20 @@ export class StockManagerParamsDto {
   private Id_Vendedor: number;
   private Id_Cliente: number;
   private Id_Almacen: number;
-
+  private Edita_Precio: boolean;
+  private Edita_Descuento: boolean;
   constructor(
     Id_Vendedor: number,
     Id_Cliente: number,
     Id_Almacen: number,
-  
+    Edita_Precio: boolean,
+    Edita_Descuento: boolean
   ) {
     this.Id_Vendedor = Id_Vendedor;
     this.Id_Cliente = Id_Cliente;
     this.Id_Almacen = Id_Almacen;
+    this.Edita_Precio = Edita_Precio;
+    this.Edita_Descuento = Edita_Descuento;
   }
 
   getIdVendedor(): number {
@@ -34,5 +38,17 @@ export class StockManagerParamsDto {
   setIdAlmacen(idAlmacen: number) {
     this.Id_Almacen = idAlmacen;
   }
- 
+
+  getEditaPrecio(): boolean {
+    return this.Edita_Precio;
+  }
+  setEditaPrecio(Edita_Precio: boolean) {
+    this.Edita_Precio = Edita_Precio;
+  }
+  getEditaDescuento(): boolean {
+    return this.Edita_Descuento;
+  }
+  setEditaDescuento(Edita_Descuento: boolean) {
+    this.Edita_Descuento = Edita_Descuento;
+  }
 }
