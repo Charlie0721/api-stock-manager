429649bfcc8716cb46863134c9f9a081b76f1b23
Carlos Casas <carloscasasramirez@gmail.comckear>
2025-03-24 10:18:15 -0500

    chean end to suscription

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,13 +2,13 @@
 export const HOST = "localhost";
 export const USER = "root";
 export const PASSWORD = "conexion1234";
-export const DATABASE = "empresa006";
+export const DATABASE = "empresa002";
 
 //# ruta para guardar los archivos texto del recolector
 export const DIRECTORYTOSAVE = "/Users/Desarrollo Carlos/Documents/";
 
 //# Fecha de finalizacion de la suscripción
-export const SUBSCRIPTION_END_DATE = "February 28 2025 12:00:00 GMT-0500";
+export const SUBSCRIPTION_END_DATE = "February 28 2026 12:00:00 GMT-0500";
 
 //#Email de envio
 export const USER_EMAIL = "carloscasasramirez@gmail.com";
